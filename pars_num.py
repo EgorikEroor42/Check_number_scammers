08f93739bfd25071e7ce6a6be2565fe626ac29d8
Egorik_Eroor42 <avdimireze@gmail.com>
2025-10-03 20:00:12 +0300

    Refactoring pars_num.py

--- pars_num.py
+++ pars_num.py
@@ -2,44 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import random
-
-num = '380637674497'
-num_rati = float(10)
-
-resp_1 = requests.get(f'https://callfilter.app/{num}')
-
-htmlcheck = BeautifulSoup(resp_1.text,'html.parser')
-
-us_1 = str(htmlcheck.find('li',class_="active"))
-us_2 = htmlcheck.find_all('h3')
-us_3 = htmlcheck.find_all('span',class_="review_comment")
-
-qua_revi = re.search(r'\d+',us_1)
-qua_revi = qua_revi.group()
-num_rati-=int(qua_revi)/10
-
-for us in us_2:
-    u = str(us.find('span',class_="reviewName"))
-    find = re.search(r'\b(' + '|'.join(['Кинули трубку','Колектори','Телефонне шахрайство',"Реклама / нав'язування послуг",'Опитування','Фінансові послуги','Магазин','Компанія','Інша']) + r')\b',u)
-    if find:
-        if find.group() in ["Реклама / нав'язування послуг",'Опитування','Інша']:
-            num_rati-=0.1
-        elif find.group() in ['Кинули трубку','Колектори','Фінансові послуги','Магазин','Компанія']:
-            num_rati-=0.2
-        else:
-            num_rati-=0.3
-
-for us in us_3:
-    find = re.search(r'\b(' + '|'.join(['Мовчали','Молчали','Тиша','Тишина','Солі','Соли','Спайси','Спайсы','Виграш','Выигрыш','Спам','Настирні','Настырные','Россиия','Росія','России','Росії','Курсы','Курси','Кредит','Кредитка','Заблокована карта','Заблокированная карта','Лохотрон','Развод']) + r')\b', str(us), flags=re.IGNORECASE)
-    if find:
-        if find.group() in ['Мовчали','Молчали','Тиша','Тишина','Спам','Настирні','Настырные']:
-            num_rati-=0.1
-        elif find.group() in ['Кредит','Кредитка','Курсы','Курси']:
-            num_rati-=0.2
-        else:
-            num_rati-=0.3
-
-
+num = str(input())
 us_ag = {
     1:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
     2:"Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
@@ -52,9 +15,36 @@ us_ag = {
     9:"Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
     10:"Mozilla/5.0 (Macintosh; Intel Mac OS X 14.6; rv:131.0) Gecko/20100101 Firefox/131.0"
 }
-
-while True:
-    resp_2 = requests.get(f'https://www.telefonnyjdovidnyk.com.ua/nomer/{num}',headers={'User-Agent':random.choice(list(us_ag.values()))})
-    if resp_2.status_code == 200:
+num_oper = ['intertelecom','kyivstar','lifecell','vodafone','3mob']
+resp_1 = requests.get(f'https://callfilter.app/{num}')
+resp_2 = requests.get(f'https://www.telefonnyjdovidnyk.com.ua/nomer/{num}',headers={'User-Agent':random.choice(list(us_ag.values()))})
+resp_3 = ''
+for oper in num_oper:
+    resp_4 = requests.get(f'https://ua.whokla.com/{oper}/{num}', headers={'User-Agent':random.choice(list(us_ag.values()))})
+    if resp_4.status_code == 200:
+        resp_3 = resp_4
         break
-print(resp_2)
+htmlcheck1 = BeautifulSoup(resp_1.text,'html.parser')
+htmlcheck2 = BeautifulSoup(resp_2.text,'html.parser')
+htmlcheck3 = BeautifulSoup(resp_3.text,'html.parser')
+txt_1 = htmlcheck1.find_all('span',class_="review_comment")
+txt_2 = htmlcheck2.find_all('p',class_="comment-text")
+txt_3 = htmlcheck3.find_all('div',class_="comment-content")
+review_arr = []
+for txt in txt_1:
+    review_arr.append(txt.get_text(strip=True))
+for txt in txt_2:
+    review_arr.append(txt.get_text(strip=True))
+for txt in txt_3:
+    review_arr.append(txt.get_text(strip=True))
+for filt in review_arr:
+    i = review_arr.index(filt)
+    filt = filt.lower()
+    filt = re.sub(r'(' + '|'.join(map(re.escape, ['(програвався запис / робот)','(розмовляв чоловік)','(німий дзвінок / скидання)','Повідомлення від адміністратора сайту telefonnyjdovidnyk.com.ua«Допоможіть іншим відвідувачам сайту тим, що поділитеся з ними своїм досвідом спілкування з цим абонентом. Коли Вам з цього номеру телефонували і як часто? Що було предметом розмови, якщо Ви підняли трубку? Коментар, який Ви відіслали, буде показаний на цьому сайті.»','Оцінка:Шахрай','Оцінка:Спамер','Оцінка:Незрозуміло','Оцінка:Безпечно','Оцінка:Корисно'])) + r')','',filt,flags=re.IGNORECASE)
+    filt = re.sub(re.compile("[""\U0001F600-\U0001F64F""\U0001F300-\U0001F5FF""\U0001F680-\U0001F6FF""\U0001F1E0-\U0001F1FF""\U00002700-\U000027BF""\U0001F900-\U0001F9FF""\U0001FA70-\U0001FAFF""\U00002600-\U000026FF""]+",flags=re.UNICODE),'',filt)
+    filt = re.sub(r'https?://\S+','',filt)
+    filt = re.sub(r'[\r\n]+', ' ', filt)
+    filt = re.sub(r'\s+', ' ', filt).strip()
+    filt = re.sub(r'[^a-za-я0-9\s]','',filt)
+    if filt != '':
+        review_arr[i] = filt
