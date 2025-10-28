18293ef50330ea7916b8846c64f307788a07f25c
Egorik_Eroor42 <avdimireze@gmail.com>
2025-10-28 22:43:19 +0200

    Added an AI model for evaluating comments.

--- pars_num.py
+++ pars_num.py
@@ -2,43 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import random
-num = str(input())
-us_ag = {
-    1:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-    2:"Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-    3:"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-    4:"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:139.0) Gecko/20100101 Chrome/138.0.0.0 Safari/537.36",
-    5:"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-    6:"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
-    7:"Mozilla/5.0 (Windows NT 11.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
-    8:"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0",
-    9:"Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0",
-    10:"Mozilla/5.0 (Macintosh; Intel Mac OS X 14.6; rv:131.0) Gecko/20100101 Firefox/131.0"
-}
+import math
+import joblib
+num = str(input('Enter number:'))
+review_arr = set()
+us_ag = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36","Mozilla/5.0 (Windows NT 11.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:139.0) Gecko/20100101 Chrome/138.0.0.0 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0","Mozilla/5.0 (Windows NT 11.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0","Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0","Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 14.6; rv:131.0) Gecko/20100101 Firefox/131.0"]
+txt_1 = requests.get(f'https://callfilter.app/{num}',headers={'User-Agent':random.choice(us_ag)})
+if txt_1.status_code == 200:
+    txt_1 = BeautifulSoup(txt_1.text,'html.parser').find_all('span',class_="review_comment")
+    if txt_1 is not None:
+        for txt in txt_1:
+            review_arr.add(txt.get_text(strip=True))
+txt_2 = requests.get(f'https://www.telefonnyjdovidnyk.com.ua/nomer/{num}',headers={'User-Agent':random.choice()})
+if txt_2.status_code == 200:
+    txt_2 = BeautifulSoup(txt_2.text,'html.parser').find('span',id='count-comments')
+    if txt_2 is not None:
+        txt_2 = math.ceil(float(re.sub('×','',txt_2.get_text(strip=True)))/12)
+    for rev in range(1 if txt_2 == 1 else txt_2):
+        txt_3 = BeautifulSoup(requests.get(f'https://www.telefonnyjdovidnyk.com.ua/nomer/{num}/p/{rev}',headers={'User-Agent':random.choice()}).text, 'html.parser').find_all('p', class_="comment-text")
+        for txt in txt_3:
+            review_arr.add(txt.get_text(strip=True))
 num_oper = ['intertelecom','kyivstar','lifecell','vodafone','3mob']
-resp_1 = requests.get(f'https://callfilter.app/{num}')
-resp_2 = requests.get(f'https://www.telefonnyjdovidnyk.com.ua/nomer/{num}',headers={'User-Agent':random.choice(list(us_ag.values()))})
-resp_3 = ''
 for oper in num_oper:
-    resp_4 = requests.get(f'https://ua.whokla.com/{oper}/{num}', headers={'User-Agent':random.choice(list(us_ag.values()))})
-    if resp_4.status_code == 200:
-        resp_3 = resp_4
-        break
-htmlcheck1 = BeautifulSoup(resp_1.text,'html.parser')
-htmlcheck2 = BeautifulSoup(resp_2.text,'html.parser')
-htmlcheck3 = BeautifulSoup(resp_3.text,'html.parser')
-txt_1 = htmlcheck1.find_all('span',class_="review_comment")
-txt_2 = htmlcheck2.find_all('p',class_="comment-text")
-txt_3 = htmlcheck3.find_all('div',class_="comment-content")
-review_arr = []
-for txt in txt_1:
-    review_arr.append(txt.get_text(strip=True))
-for txt in txt_2:
-    review_arr.append(txt.get_text(strip=True))
-for txt in txt_3:
-    review_arr.append(txt.get_text(strip=True))
+    txt_4 = requests.get(f'https://ua.whokla.com/{oper}/{num}',headers={'User-Agent':random.choice()})
+    if txt_4.status_code == 200:
+        txt_4 = BeautifulSoup(txt_4.text,'html.parser').find_all('div',class_='comment-content')
+        for txt in txt_4:
+            review_arr.add(txt.get_text(strip=True))
+comment_score_arr = {}
+score_ai = joblib.load(r'C:\Users\avdim\Desktop\MyApps\CN\ail\AI.joblib')
 for filt in review_arr:
-    i = review_arr.index(filt)
     filt = filt.lower()
     filt = re.sub(r'(' + '|'.join(map(re.escape, ['(програвався запис / робот)','(розмовляв чоловік)','(німий дзвінок / скидання)','Повідомлення від адміністратора сайту telefonnyjdovidnyk.com.ua«Допоможіть іншим відвідувачам сайту тим, що поділитеся з ними своїм досвідом спілкування з цим абонентом. Коли Вам з цього номеру телефонували і як часто? Що було предметом розмови, якщо Ви підняли трубку? Коментар, який Ви відіслали, буде показаний на цьому сайті.»','Оцінка:Шахрай','Оцінка:Спамер','Оцінка:Незрозуміло','Оцінка:Безпечно','Оцінка:Корисно'])) + r')','',filt,flags=re.IGNORECASE)
     filt = re.sub(re.compile("[""\U0001F600-\U0001F64F""\U0001F300-\U0001F5FF""\U0001F680-\U0001F6FF""\U0001F1E0-\U0001F1FF""\U00002700-\U000027BF""\U0001F900-\U0001F9FF""\U0001FA70-\U0001FAFF""\U00002600-\U000026FF""]+",flags=re.UNICODE),'',filt)
@@ -47,4 +40,4 @@ for filt in review_arr:
     filt = re.sub(r'\s+', ' ', filt).strip()
     filt = re.sub(r'[^a-za-я0-9\s]','',filt)
     if filt != '':
-        review_arr[i] = filt
+        comment_score_arr[f'{filt}'] = score_ai.predict([filt])
